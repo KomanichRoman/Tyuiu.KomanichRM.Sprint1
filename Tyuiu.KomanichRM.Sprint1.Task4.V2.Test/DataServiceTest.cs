ea0d557f647e21e5c6b94ed24ce29534a1025f29
Tyuiu.KomanichRM@outlook.com <gorotro_215@mail.ru>
2023-09-23 18:50:39 +0500

    Task4

---
+++ Tyuiu.KomanichRM.Sprint1.Task4.V2.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KomanichRM.Sprint1.Task4.V2.Lib;
+
+namespace Tyuiu.KomanichRM.Sprint1.Task4.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double y = 16;
+            double x = 32;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(res, 0.125);
+        }
+    }
+}
---
+++ Tyuiu.KomanichRM.Sprint1.Task4.V2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KomanichRM.Sprint1.Task4.V2.Lib;
+
+namespace Tyuiu.KomanichRM.Sprint1.Task4.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите переменную X:");
+            double x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите переменную Y:");
+            double y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("1 / sqrt(x + (2 * y) = " + ds.Calculate(x, y));
+            Console.ReadKey();
+        }
+    }
+}
